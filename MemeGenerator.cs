b69db732f1ce042ce64a1cad18ee1ca5101bc045
Ben Highsted <49290301+BenHighsted@users.noreply.github.com>
2020-02-24 01:35:36 +1300

    Added support for four templates
    Can have any amount of source images.

--- MemeGenerator.cs
+++ MemeGenerator.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Drawing;
 using System.IO;
+using System.Collections.Generic;
 
 
 
@@ -20,6 +21,7 @@ namespace MemeGenerator
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>();
             System.Console.WriteLine("Generating...");
 
             //instance of Random that I will be using; must use the same one to keep it random.
@@ -33,48 +35,53 @@ namespace MemeGenerator
 
             //These contain the sizes that the source images will need to be to fit into the selected template.
             //i.e. template one (templates[0]) source images need to be 200, 300 (height[0] and width[0])
-            int[] height = new int[]{200, 300, 400, 300};
-            int[] width = new int[]{200, 300, 400, 300};
+            int[] height = new int[]{300, 85, 300, 260, 200, 200};
+            int[] width = new int[]{280, 85, 300, 240, 200, 200};
 
             //These are the arrays (2D) to hold the positions for the images.
             int length = templates.Length;
 
-            int[,] x = new int[,]{{12, 30, 0, 0, 0, 0},{12, 30, 40, 50, 60, 70},{12, 12, 0, 0, 0, 0}};
-            int[,] y = new int[,]{{12, 30, 0, 0, 0, 0},{12, 30, 40, 50, 60, 70},{12, 12, 0, 0, 0, 0}};
+            int[,] x = new int[,]{{545, 545, 0, 0, 0, 0},{120, 310, 0, 0, 0, 0},{300, 300, 0, 0, 0, 0},{125, 535, 940, 125, 535, 940}};
+            int[,] y = new int[,]{{55, 435, 0, 0, 0, 0},{92, 60, 0, 0, 0, 0},{0, 300, 0, 0, 0, 0},{200, 200, 200, 590, 590, 590}};
 
             //the number of images on the specified template
-            int[] number_of_images = new int[]{2, 2, 2, 4};
+            int[] number_of_images = new int[]{2, 2, 2, 6};
             System.Console.WriteLine(number_of_images[templateNum]);
+
+            Graphics g = Graphics.FromImage(template); //places template
             
-            
+            //reads in images
             var images = Directory.GetFiles("images", "*.jpg");
-            int imageNum = rand.Next(images.Length);
 
             //plan is to call an image, resize it, draw it, then move onto the next one.
+            
             for(int i = 0; i < number_of_images[templateNum]; i++){
+                bool duplicate = false;
+                int imageNum = rand.Next(images.Length);
+
+                if (!list.Contains(imageNum)){
+                    list.Add(imageNum);
+                } else {
+                    //duplicate number
+                    i = i - 1; //not sure if this works yet, but hopefully will add 1 more loop every duplicate
+                    duplicate = true;
+                }
+                //now that we know it hasnt been generated yet, we can continue
                 
+                //so the process is read the image, resize it, draw it.
+                if(!duplicate){
+                    Image source_image = Image.FromFile(images[imageNum]);
+                    source_image = resizeImage(source_image, height[templateNum], width[templateNum]);
+                    g.DrawImage(source_image, x[templateNum, i], y[templateNum, i]);
+                }
             }
-
-            Image source_image = Image.FromFile(images[imageNum]);
-
-            //attempting to resize image here (image, height, width)
-            source_image = resizeImage(source_image, 500, 500);
-
-            //create a graphics object using the template // Image.FromFile(templates[1])
-            Graphics g = Graphics.FromImage(template);
-
-            //draw the source image onto the template (image, x, y)
-            g.DrawImage(source_image, 0, 0);
-
-            //save the image under the name 'result'
             template.Save("result.jpg");
 
             System.Console.WriteLine("Meme created!");
         }
 
         /** Method used to resize an image to a specified height and width */
-        public static Image resizeImage(Image image, int new_height, int new_width)
-        {
+        public static Image resizeImage(Image image, int new_height, int new_width){
             Bitmap new_image = new Bitmap(new_width, new_height);
             Graphics g = Graphics.FromImage((Image)new_image );
             g.DrawImage(image, 0, 0, new_width, new_height);
